35cbbc143d472fb5ed2629add416f6e883b005a5
Pascal.Stodieck <stodieck.p@gmail.com>
2023-11-24 10:36:37 +0100

    loco

--- db/migrate/20231122112112_add_columns_to_appointments.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnsToAppointments < ActiveRecord::Migration[7.1]
-  def change
-    add_column :appointments, :date, :datetime
-    add_column :appointments, :cause, :text
-    add_column :appointments, :urgent, :boolean, default: false
-    add_reference :appointments, :doctor, foreign_key: true
-    add_reference :appointments, :patient, foreign_key: true
-  end
-end
